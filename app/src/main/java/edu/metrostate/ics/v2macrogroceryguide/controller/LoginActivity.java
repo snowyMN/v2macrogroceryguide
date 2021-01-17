1cc31cfc27912847ea90eb447f6eadf772f3b46c
wuzzey <mfuzzey101@gmail.com>
2021-01-17 14:22:25 -0600

    Adding UI LoginActivity - messing with a few different layout - general consensus with Android developers seems to be to move to constraint layout (but just do it better than I did in the last project haha)

---
+++ app/src/main/java/edu/metrostate/ics/v2macrogroceryguide/controller/LoginActivity.java
@@ -0,0 +1,14 @@
+package edu.metrostate.ics.v2macrogroceryguide.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
